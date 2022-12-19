2e7848bf87f1cb3b47b819fee417a446e2807f85
Alberto Rivera <albertorivera@MacBook-Pro-de-Alberto.local>
2022-12-19 12:06:09 +0100

    feat: add new content react

---
+++ Modulo-2/1-react/1-code-example/mui-react/src/App.jsx
@@ -0,0 +1,16 @@
+import BasicButtonGroup from './components/BasicButtonGroup'
+import BasicButtons from './components/BasicButtons'
+import BasicTable from './components/BasicTable'
+
+function App() {
+
+  return (
+    <div>
+      <BasicButtons />
+      <BasicButtonGroup />
+      <BasicTable />
+    </div>
+  )
+}
+
+export default App
