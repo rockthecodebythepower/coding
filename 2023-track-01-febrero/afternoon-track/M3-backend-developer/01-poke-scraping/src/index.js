43f6ac3457c7d40b68ab4101a24877f8e1ac7f70
Ccastillo06 <ccastillon92@gmail.com>
2023-02-09 20:41:47 +0100

    master class 9 febrero - pokemon scraper terminado

--- 2023-track-01-febrero/afternoon-track/M3-backend-developer/01-poke-scraping/src/index.js
+++ 2023-track-01-febrero/afternoon-track/M3-backend-developer/01-poke-scraping/src/index.js
@@ -1,6 +1,13 @@
+// //////////////////////////////////
+// Objectivos DIA 1
 // Entrar a la página https://pokemondb.net/pokedex/national de forma automatizada ✅
 // Sacar un pokemon de la lista de la página y convertirlo en un objeto de JS ✅
 // Guardar ese pokemon en un archivo .json (que actúe como nuestra DB) ✅
+// //////////////////////////////////
+// Objetivos DIA 2
+// Recuperar los datos de todos los pokemon de la web y crear un array con todos esos datos formateados ✅
+// Guardar todos los pokemon en un objeto para subirlos a una DB ✅
+// Crear una DB (database) MongoDB (no relacional) y guardar ahí toda la información
 // //////////////////////////////////
 // import puppeteer from 'puppeteer';
 // Por defecto NODE no soporta el uso de import ... from '...'
@@ -24,33 +31,63 @@ const main = async () => {
   await page.goto('https://pokemondb.net/pokedex/national');
 
   const pokemonCardSelector = 'div.infocard';
-  const pokemonCards = await page.waitForSelector(pokemonCardSelector);
+  await page.waitForSelector(pokemonCardSelector); // document.querySelector()
+
+  // D2 - Usamos $$eval porque es como hacer .querySelectorAll()
+  const pokemonList = await page.$$eval(pokemonCardSelector, (elements) => {
+    // Hacemos spread operator de elements porque son un NodeList y no soportan .map
+    // Al copiarlos en un nuevo Array con el spread, si soportan .map
+    const elementsAsArray = [...elements];
+
+    // Formateamos todos los pokemon en un objeto cada uno de ellos
+    const allPokemon = elementsAsArray.map((el) => {
+      const imgSrc = el.querySelector('a img')?.src || el.querySelector('a span').dataset.src;
+      const pokemonText = el.textContent;
+      const splitPokemon = pokemonText.split(' ');
+      const number = splitPokemon[0].replace('#', '');
+      const name = splitPokemon[1];
+      const types = [splitPokemon[2], splitPokemon[4]].filter((value) => !!value);
+
+      const pokemonData = {
+        name,
+        number,
+        imageUrl: imgSrc,
+        types,
+      };
+
+      return pokemonData;
+    });
+
+    return allPokemon;
+  });
 
   // El código de la función .evaluate corre solamente dentro del navegador "fantasma"
-  const pokemon = await pokemonCards.evaluate((el) => {
-    const imgSrc = el.querySelector('a img').src;
-    const pokemonText = el.textContent;
-    const splitPokemon = pokemonText.split(' ');
-    const number = splitPokemon[0].replace('#', '');
-    const name = splitPokemon[1];
-    const types = [splitPokemon[2], splitPokemon[4]].filter((value) => !!value);
+  // const pokemon = await pokemonCards.evaluate((el) => {
+  //   const imgSrc = el.querySelector('a img').src;
+  //   const pokemonText = el.textContent;
+  //   const splitPokemon = pokemonText.split(' ');
+  //   const number = splitPokemon[0].replace('#', '');
+  //   const name = splitPokemon[1];
+  //   const types = [splitPokemon[2], splitPokemon[4]].filter((value) => !!value);
 
-    const pokemonData = {
-      name,
-      number,
-      imageUrl: imgSrc,
-      types,
-    };
+  //   const pokemonData = {
+  //     name,
+  //     number,
+  //     imageUrl: imgSrc,
+  //     types,
+  //   };
 
-    return pokemonData;
-  });
+  //   return pokemonData;
+  // });
 
   // Cierra el navegador "fantasma"
   await browser.close();
 
-  console.log('Guardando pokemon:', pokemon);
+  console.log('Guardando todos los pokemon:', pokemonList);
+  // console.log('Guardando pokemon:', pokemon);
   // Transformo el objeto pokemon a un texto y lo guardo en un archivo
-  await fs.writeFile('./data/pokemon.json', JSON.stringify(pokemon, null, 2));
+  await fs.writeFile('./data/pokemon.json', JSON.stringify(pokemonList, null, 2));
+  // await fs.writeFile('./data/pokemon.json', JSON.stringify(pokemon, null, 2));
 };
 
 // fetch()
