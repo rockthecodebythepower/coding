7d9c645267aada5fa8c8a93c344db1a92d28b33f
Ccastillo06 <ccastillon92@gmail.com>
2023-03-17 20:54:54 +0100

    masterclass 17 marzo 2024 M3 afternoon track - sockets

--- 2023-track-01-febrero/afternoon-track/M3-backend-developer/06-socket-game/client/src/App.jsx
+++ 2023-track-01-febrero/afternoon-track/M3-backend-developer/06-socket-game/client/src/App.jsx
@@ -1,21 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
 // Nos conectamos por Sockets al servidor que hayamos creado
 const socket = io('http://localhost:4001');
+// const socket = io('https://e582-5-224-255-55.eu.ngrok.io/ping');
 
 function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
   const [joined, setJoined] = useState(false);
   const [players, setPlayers] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const usernameRef = useRef();
+  const roomRef = useRef();
+
+  // Este useEffect me sirve para suscribirme a eventos del socket cuando cargue la app
   useEffect(() => {
     socket.on('joined', (data) => {
       console.log('Se han unido a la room:', data);
       setJoined(true);
       setPlayers(data.players);
+    });
+
+    // Escuchamos cuando un player se vaya
+    socket.on('left', (data) => {
+      setPlayers(data.players);
+    });
+
+    // Si el servidor nos avisa de que la room está llena, mostramos un error
+    // Y vaciamos el input de la habitación
+    socket.on('join_error', () => {
+      setError(true);
+      setRoom('');
+    });
+
+    // Escuchamos cuando haya un resultado
+    socket.on('result', (data) => {
+      const { winner } = data;
+
+      console.log('Resultado:', { winner });
+
+      if (!winner) {
+        alert('Habéis empatado!');
+      } else if (winner === usernameRef.current) {
+        alert('Has ganado 🎉!');
+      } else {
+        alert('Has perdido ❌!');
+      }
+
+      setLoading(false);
+    });
+
+    window.addEventListener('beforeunload', (event) => {
+      event.preventDefault();
+      socket.emit('leave_room', { username: 'TEST', room: 'TEST' });
     });
   }, []);
 
@@ -30,6 +71,21 @@ function App() {
       username,
       room,
     });
+
+    // De esta forma podemos usar el username y room del player
+    // en cualquier momento y estarán sincronizados
+    usernameRef.current = username;
+    roomRef.current = room;
+  };
+
+  const handleChoice = (choice) => {
+    socket.emit('player_action', {
+      username,
+      room,
+      choice,
+    });
+
+    setLoading(true);
   };
 
   return (
@@ -41,6 +97,17 @@ function App() {
           <h2>Te has unido a la habitación {room}</h2>
           <h3>Tu nombre de usuario es {username}</h3>
 
+          <h3>Selecciona una opción:</h3>
+          <button disabled={loading} onClick={() => handleChoice('rock')}>
+            Piedra 🥌
+          </button>
+          <button disabled={loading} onClick={() => handleChoice('paper')}>
+            Papel 📄
+          </button>
+          <button disabled={loading} onClick={() => handleChoice('scissors')}>
+            Tijeras ✂
+          </button>
+
           <h3>Jugadores en partida:</h3>
           <ul>
             {players.map((player) => (
@@ -49,37 +116,41 @@ function App() {
           </ul>
         </>
       ) : (
-        <form onSubmit={handleJoinRoom}>
-          <label htmlFor="username">
-            <p>Nombre de usuario</p>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              value={username}
-              onChange={(ev) => {
-                setUsername(ev.target.value);
-              }}
-            />
-          </label>
+        <div>
+          {error ? <h3>🔴 Habitación llena, únete o crea una nueva habitación 🔴</h3> : null}
 
-          <label htmlFor="room">
-            <p>Nombre de habitación</p>
-            <input
-              type="text"
-              name="room"
-              id="room"
-              value={room}
-              onChange={(ev) => {
-                setRoom(ev.target.value);
-              }}
-            />
-          </label>
+          <form onSubmit={handleJoinRoom}>
+            <label htmlFor="username">
+              <p>Nombre de usuario</p>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={username}
+                onChange={(ev) => {
+                  setUsername(ev.target.value);
+                }}
+              />
+            </label>
 
-          <br />
+            <label htmlFor="room">
+              <p>Nombre de habitación</p>
+              <input
+                type="text"
+                name="room"
+                id="room"
+                value={room}
+                onChange={(ev) => {
+                  setRoom(ev.target.value);
+                }}
+              />
+            </label>
 
-          <button type="submit">Conectar</button>
-        </form>
+            <br />
+
+            <button type="submit">Conectar</button>
+          </form>
+        </div>
       )}
     </div>
   );
--- 2023-track-01-febrero/afternoon-track/M3-backend-developer/06-socket-game/server/index.js
+++ 2023-track-01-febrero/afternoon-track/M3-backend-developer/06-socket-game/server/index.js
@@ -1,8 +1,17 @@
 import http from 'http';
 import express from 'express';
+import cors from 'cors';
 import { Server } from 'socket.io';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+  })
+);
 
 app.get('/ping', (_req, res) => {
   res.status(200).json({ data: 'Pong!' });
@@ -14,7 +23,10 @@ const server = http.createServer(app);
 const io = new Server(server, {
   // Configuro CORS para que cualquier cliente pueda conectarse
   cors: {
-    origin: true,
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    methods: ['GET', 'POST'],
   },
 });
 
@@ -24,6 +36,8 @@ const rooms = {};
 // Configuramos aquí todo el comportamiento de un cliente al servidor por sockets
 io.on('connection', (client) => {
   console.log('>>> Cliente conectado');
+  let connectionUsername = '';
+  let connectionRoom = '';
 
   client.on('join_room', (data) => {
     console.log('>>> Uniéndose a la ROOM:', data);
@@ -33,8 +47,17 @@ io.on('connection', (client) => {
     if (!rooms[room]) {
       rooms[room] = {
         players: [],
+        choices: {},
       };
     }
+
+    // Limitar a 2 el número máximo de clientes por cada ROOM
+    if (rooms[room].players.length >= 2) {
+      client.emit('join_error');
+      return;
+    }
+
+    // 🚀🚀🚀 Nice to have: Hacer que ninguno de los dos players pueda tener el mismo nombre
 
     // Internamente, socket.io crea sus propias ROOM (donde no podemos guardar datos)
     client.join(room);
@@ -42,14 +65,77 @@ io.on('connection', (client) => {
     // A partir de aquí ya existirá siempre la room a la que se ha conectado el user
     rooms[room].players.push(username);
 
+    // Añadimos valores a las variables de la conexión del usuario
+    connectionUsername = username;
+    connectionRoom = room;
+
     // Cuando un jugador se termine de unir a un ROOM, avisamos a los clientes conectados
     io.to(room).emit('joined', { username, room, players: rooms[room].players });
   });
 
-  client.on('disconnect', () => {
-    console.log('>>> Cliente desconectado');
+  client.on('player_action', (data) => {
+    const { username, room, choice } = data;
+    rooms[room].choices[username] = choice;
 
-    // Aquí tenemos que limpiar la room cuando alguien se desconecte
+    // Con esto comprobamos que hay dos elecciones hechas y podemos comprobar si
+    // algún player ha ganado la partida 🚀
+    const playersWithChoice = Object.entries(rooms[room].choices);
+    if (playersWithChoice.length === 2) {
+      // Sacamos el nombre y elección de cada jugador
+      const player0 = playersWithChoice[0][0];
+      const player0Choice = playersWithChoice[0][1];
+
+      const player1 = playersWithChoice[1][0];
+      const player1Choice = playersWithChoice[1][1];
+
+      console.log('>>> Comprobando ganador:', {
+        player0,
+        player0Choice,
+        player1,
+        player1Choice,
+      });
+
+      // Comprobamos que han empatado
+      if (player0Choice === player1Choice) {
+        io.to(room).emit('result', {
+          winner: null,
+        });
+      }
+      // Comprobamos que gana el player 0
+      else if (
+        (player0Choice === 'rock' && player1Choice === 'scissors') ||
+        (player0Choice === 'paper' && player1Choice === 'rock') ||
+        (player0Choice === 'scissors' && player1Choice === 'paper')
+      ) {
+        io.to(room).emit('result', {
+          winner: player0,
+        });
+      }
+      // En caso contrario, gana el player 1
+      else {
+        io.to(room).emit('result', {
+          winner: player1,
+        });
+      }
+
+      // Limpiamos los choices después de haber comprobado ganador en la partida
+      rooms[room].choices = {};
+    }
+  });
+
+  client.on('disconnect', (data) => {
+    console.log('>>> Cliente desconectado', data);
+
+    // Avisar a los jugadores de cada ROOM cuando alguien se vaya
+    // Saco de la ROOM al PLAYER que tenga el nombre USERNAME que me ha enviado
+    if (rooms[connectionRoom]?.players?.length) {
+      rooms[connectionRoom].players = rooms[connectionRoom].players.filter((player) => {
+        return player !== connectionUsername;
+      });
+
+      // Mando la lista de players actualizada al FE
+      io.to(connectionRoom).emit('left', { players: rooms[connectionRoom].players });
+    }
   });
 });
 
